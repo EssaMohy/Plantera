563c85e63fc31d10160cdb4de57337654a3045a8
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2025-04-12 20:40:35 +0200

    update lightScreen.jsx
    Updated UI for LightSreen  with improved layout and styling

--- screens/LightScreen.jsx
+++ screens/LightScreen.jsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import React from "react";
+
 import LightSensorComponent from "../components/LightSensorComponent";
 
 const SettingsScreen = () => {
-  return <LightSensorComponent />;
+  return (
+    <ImageBackground
+      source={require("../assets/images/7.png")}      
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <LightSensorComponent />
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default SettingsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+    backgroundColor:"white"
+    
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
